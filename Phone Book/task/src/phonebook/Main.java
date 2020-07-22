cc419001a86fa1945b364a08185569b9d9cbe2d8
Kevin R <seidaikevin@gmail.com>
2020-07-21 19:45:52 -0500

    Refactor and formatting

--- Phone Book/task/src/phonebook/Main.java
+++ Phone Book/task/src/phonebook/Main.java
@@ -1,76 +1,66 @@
 package phonebook;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String pathToDirectory = "C:\\Users\\kevin\\random\\directory.txt";
-        String pathToFind = "C:\\Users\\kevin\\random\\find.txt";
-        File fileDirectory = new File(pathToDirectory);
-        File fileFind = new File(pathToFind);
         List<String> directory = new ArrayList<>();
         List<String> toFinds = new ArrayList<>();
+        Duration bubbleSortDuration = new Duration();
+        Duration jumpSearchDuration = new Duration();
+        Duration linearSearchDuration = new Duration();
 
-        try (Scanner scanner = new Scanner(fileDirectory);
-             Scanner scanner1 = new Scanner(fileFind)) {
-            while (scanner.hasNext()) {
-                directory.add(scanner.nextLine());
-            }
-            while (scanner1.hasNext()) {
-                toFinds.add(scanner1.nextLine());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("No file found");
+        int counter;
+
+        try {
+            directory = Files.readAllLines(Path.of("C:\\Users\\kevin\\random\\directory.txt"));
+            toFinds = Files.readAllLines(Path.of("C:\\Users\\kevin\\random\\find.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        int counter = 0;
+        counter = 0;
         System.out.println("Start searching (linear search)...");
-
-        Duration linearSearchDuration = new Duration();
         linearSearchDuration.start();
         for (String person : toFinds) {
-                if (linearSearch(directory,person)!=-1) {
-                    counter++;
-                }
+            if (linearSearch(directory, person) != -1) {
+                counter++;
+            }
         }
         linearSearchDuration.stop();
-        System.out.printf("Found %d / 500 entries. Time taken: %s%n",counter,linearSearchDuration);
-        counter=0;
+        System.out.printf("Found %d / 500 entries. Time taken: %s%n", counter, linearSearchDuration);
+        counter = 0;
         System.out.println("Start searching (bubble sort + jump search)...");
 
-        Duration bubbleSortDuration = new Duration();
-        if (bubbleSort(directory,bubbleSortDuration, linearSearchDuration.getMilliseconds()*10)){
-            Duration jumpSearchDuration = new Duration();
+        if (bubbleSort(directory, bubbleSortDuration, linearSearchDuration.getMilliseconds() * 10)) {
             jumpSearchDuration.start();
             for (String person : toFinds) {
-                if (jumpSearch(directory,person)!=-1) {
+                if (jumpSearch(directory, person) != -1) {
                     counter++;
                 }
             }
             jumpSearchDuration.stop();
-            System.out.printf("Found %d / 500 entries. Time taken: %s%n",counter,
-                    new Duration(bubbleSortDuration.getMilliseconds()+jumpSearchDuration.getMilliseconds()));
-            System.out.printf("Sorting time: %s%n",bubbleSortDuration);
-            System.out.printf("Searching time: %s%n",jumpSearchDuration);
-        }else {
-            Duration linearSearchDuration2 = new Duration();
-            linearSearchDuration2.start();
+            System.out.printf("Found %d / 500 entries. Time taken: %s%n", counter,
+                    new Duration(bubbleSortDuration.getMilliseconds() + jumpSearchDuration.getMilliseconds()));
+            System.out.printf("Sorting time: %s%n", bubbleSortDuration);
+        } else {
+            jumpSearchDuration.start();
             for (String person : toFinds) {
-                if (linearSearch(directory,person)!=-1) {
+                if (linearSearch(directory, person) != -1) {
                     counter++;
                 }
             }
-            linearSearchDuration2.stop();
-            System.out.printf("Found %d / 500 entries. Time taken: %s%n",counter,
-                    new Duration(bubbleSortDuration.getMilliseconds()+linearSearchDuration2.getMilliseconds()));
-            System.out.printf("Sorting time: %s - STOPPED, moved to linear search %n",bubbleSortDuration);
-            System.out.printf("Searching time: %s%n",linearSearchDuration2);
+            jumpSearchDuration.stop();
+            System.out.printf("Found %d / 500 entries. Time taken: %s%n", counter,
+                    new Duration(bubbleSortDuration.getMilliseconds() + jumpSearchDuration.getMilliseconds()));
+            System.out.printf("Sorting time: %s - STOPPED, moved to linear search %n", bubbleSortDuration);
         }
+        System.out.printf("Searching time: %s%n", jumpSearchDuration);
     }
 
     public static int jumpSearch(List<String> array, String target) {
@@ -121,17 +111,23 @@ public class Main {
         return -1;
     }
 
-    public static boolean bubbleSort(List<String> list, Duration duration, long maxTime) throws RuntimeException {
+    /**
+     * @param list List to be sorted
+     * @param duration Duration object for timestamps
+     * @param maxTime Maximum time taken
+     * @return Returns whether it finishes sorting
+     */
+    public static boolean bubbleSort(List<String> list, Duration duration, long maxTime) {
         duration.start();
         for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = 0; j < list.size() - i -1; j++) {
-                if (list.get(j).charAt(9) > list.get(j + 1).charAt(9)){
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j).charAt(9) > list.get(j + 1).charAt(9)) {
                     String temp = list.get(j);
-                    list.set(j,list.get(j+1));
-                    list.set(j+1,temp);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
                 }
             }
-            if (duration.getMillisSinceStart() > maxTime){
+            if (duration.getMillisSinceStart() > maxTime) {
                 duration.stop();
                 return false;
             }
@@ -139,6 +135,7 @@ public class Main {
         duration.stop();
         return true;
     }
+
     public static int linearSearch(List<String> arrayList,
                                    String string) {
         int index = -1;
